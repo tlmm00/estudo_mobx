da0fa3eecb879a0b4f6f0bd45d2aac0231c42c50
tlmm00 <tluigimm@gmail.com>
2024-04-22 17:44:22 -0300

    primeiro commit corrigido

---
+++ ex_mobx/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/tlmm/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/tlmm/Codes/estudo_mobx/ex_mobx" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/tlmm/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/tlmm/Codes/estudo_mobx/ex_mobx"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9jNGNkNDhlMTg2NDYwYjMyZDQ0NTg1Y2UzYzEwMzI3MWFiNjc2MzU1Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/tlmm/Codes/estudo_mobx/ex_mobx/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/tlmm/Codes/estudo_mobx/ex_mobx/lib/main.dart"
+)
